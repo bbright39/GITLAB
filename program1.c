23c1634dc324e4ccee0fe4c41c271ef0f16da6db
bbright39 <bright_dall@hotmail.com>
2017-04-27 15:29:40 +0700

    change

--- program1.c
+++ program1.c
@@ -2,7 +2,7 @@
 void main()
 {
   char Name[30][40] = {"James Jones", "Mary McBeth", "Ying Phakpon","Chai Chanchai", "Yong Boonsith", "Kitti Karndee"}; 
-  int ID[30] = {1,2, 3, 5304, 5305, 5306}; 
+  int ID[30] = {1,2, 3,4, 5305, 5306}; 
   float GPA[30] = {3.73, 2.35, 3.65, 3.24, 1.32, 3.26}; 
   int N = 6, i;
   int SearchID, found_index; 
